dc3d97b24a7cbf8a98165d219d4dea926bd30b57
Mehdi Charef <me-charef@hotmail.fr>
2023-03-14 20:02:52 +0100

    added font

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
